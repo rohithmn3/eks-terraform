6b15c01c64e8c7d755f09875a31e699cf11063ab
i346327 <rohith.m.n@sap.com>
2020-06-08 19:11:46 +0530

    increase the number of nodes: as t2.micro max 4 pods per node

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -46,8 +46,8 @@ resource "aws_eks_node_group" "demo" {
   instance_types  = ["t2.micro"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 2
     min_size     = 1
   }
 
